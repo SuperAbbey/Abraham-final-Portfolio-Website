28e9be460ff61d724df0998697b7882c3b38cda1
SuperAbbey <abrahamramirezsierra@gmail.com>
2025-06-13 00:21:55 -0700

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -106,10 +106,10 @@ export const HeroSection = () => {
                         border-white bg-white text-gray-900 h-12 px-6 rounded-xl
                          animate-bounce [animation-duration:1.5s]">
                         <span>⚽️ 🥺</span>
-                        <span className="font-semibold animate-bounce [animation-duration:1.5s]">Let's Connect!</span>
+                        <span className="font-semibold animate-bounce [animation-duration:1.5s]">Lets Connect!</span>
                     </button>
                 </div>
             </div>
         </div>
     );// might have to change to ;
-};
+}
