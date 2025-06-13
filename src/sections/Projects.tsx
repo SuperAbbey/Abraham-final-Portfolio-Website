f5731b07490194b05dd5eba79c37e2ddd26f1609
SuperAbbey <abrahamramirezsierra@gmail.com>
2025-06-13 00:36:11 -0700

    Update Projects.tsx

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -42,7 +42,7 @@ const portfolioProjects = [
             { title: "Improved site speed by 50%" },
             { title: "Increased mobile traffic by 35%" },
         ],
-        link: "https://youtu.be/Z7I5uSRHMHg",
+        link: "https://frontend-tribe-ai-startup-landing-f-ten.vercel.app/#",
         image: aiStartupLandingPage,
     },
 ];
