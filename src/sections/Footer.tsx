bbdeddf7a553c01231e34c3a7a226341c0294107
SuperAbbey <abrahamramirezsierra@gmail.com>
2025-06-16 16:06:44 -0500

    Update Footer.tsx

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,12 +1,12 @@
 import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
 const footerLinks = [
   {
-    title: 'Gmail',
-    href: 'https://www.youtube.com/@AbrahamRamirezSierra',
+    title: 'About',
+    href: 'https://personal-site-ah8k551fw-abraham-s-projects-d2831a71.vercel.app/about',
   },
   {
     title: 'Resume',
-    href: 'https://plum-viviana-14.tiiny.site',
+    href: 'https://personal-site-ah8k551fw-abraham-s-projects-d2831a71.vercel.app/resume',
   },
   {
     title: 'Instagram',
@@ -14,43 +14,34 @@ const footerLinks = [
   },
   {
     title: 'Linkedin',
-    href: 'https://www.linkedin.com/in/abrahamram',
+    href: 'www.linkedin.com/in/abrahamram',
   },
 
 ];
 export const Footer = () => {
   return(
 
-      <footer className="relative z-10 overflow-x-clip pointer-events-auto">
-        <div
-            className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-yellow-300/30
-  [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10 pointer-events-none"
-        ></div>
+    <footer className="relative -z-10 overflow-x-clip">
+      <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-yellow-300/30
+      [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
+      <div className="container">
+        <div className="border-t border-white/15 py-6 text-sm
+         flex flex-col md:flex-row md:justify-between items-center gap-8">
+          <div className="text-white/40">&copy; 2025. All Rights Reserved</div>
+          <nav className="flex flex-col md:flex-row items-center gap-8">
+            {footerLinks.map(link =>(
+                <a href="#" key={link.title}
+                className="inline-flex items-center gap-1.5">
+                  <span className="font-bold">{link.title}</span>
+                  <ArrowUpRightIcon className="size-5"/>
+                </a>
+            ))}
 
+          </nav>
 
-        <div className="container">
-          <div className="border-t border-white/15 py-6 text-sm
-    flex flex-col md:flex-row md:justify-between items-center gap-8">
-            <div className="text-white/40">&copy; 2025. All Rights Reserved</div>
-
-            <nav className="flex flex-col md:flex-row items-center gap-8">
-              {footerLinks.map(link => (
-                  <a
-                      key={link.title}
-                      href={link.href}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="inline-flex items-center gap-1.5 text-white/80 hover:text-sky-300 transition-colors duration-200 group"
-                  >
-                    <span className="font-bold">{link.title}</span>
-                    <ArrowUpRightIcon className="size-5 group-hover:animate-bounce" />
-                  </a>
-              ))}
-            </nav>
-          </div>
         </div>
-      </footer>
-
+        </div>
+    </footer>
 
   );
 };
